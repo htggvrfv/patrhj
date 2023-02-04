cde6ef02d6d21a89f982c0912403b417de020251
htggvrfv <96030056+htggvrfv@users.noreply.github.com>
2023-02-04 03:26:59 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Patr-keepalive
+
+利用Github Actions，定时重启Patr云的部署以让应用稳定运行
+
+## 部署说明
+
+* Fork 这个项目，然后启动
+* 项目用到的环境变量
+  | 变量名 | 是否必须 | 默认值 | 备注 |
+  | ------------ | ------ | ------ | ------ |
+  | PATR_USERNAME | 是 | 无 | Patr 账户登录邮箱 |
+  | PATR_PASSWORD | 是 | 无 | Patr 账户登录密码 |
+
+![image](https://user-images.githubusercontent.com/116990986/211692321-34df154a-320a-448f-9abe-2efab9c53550.png)
+
+## 免责声明
+
+* 本程序仅供学习了解, 非盈利目的，请于下载后 24 小时内删除, 不得用作任何商业用途, 文字、数据及图片均有所属版权, 如转载须注明来源。
+* 使用本程序必循遵守部署免责声明。使用本程序必循遵守部署服务器所在地、所在国家和用户所在国家的法律法规, 程序作者不对使用者任何不当行为负责.
+
+## 赞助
+
+爱发电：https://afdian.net/a/Misaka-blog
+
+![afdian-MisakaNo の 小破站](https://user-images.githubusercontent.com/122191366/211533469-351009fb-9ae8-4601-992a-abbf54665b68.jpg)
---
+++ patr.py
@@ -0,0 +1,58 @@
+﻿import os
+import time
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+# 使用环境变量，并预留本地调试接口
+try:
+    username = os.environ['PATR_USERNAME']
+except:
+    username = "iopt"
+
+try:
+    password = os.environ['PATR_PASSWORD']
+except:
+    password = "DD2Klf2XEAf6zfvuf(&^$%^F *gynozaON(*&^%$etygudi56"
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    print("Step 1: Account login")
+    page.goto("https://app.patr.cloud/login")
+    page.get_by_placeholder("Username/Email").click()
+    page.get_by_placeholder("Username/Email").fill(username)
+    page.get_by_placeholder("Password").click()
+    page.get_by_placeholder("Password").fill(password)
+    page.screenshot(path=f'result1.png')
+    # 预留30s，以等待页面缓冲
+    time.sleep(30)
+    page.get_by_role("button", name="LOGIN").click()
+    page.screenshot(path=f'result2.png')
+    # 预留30s，以进入控制台首页
+    time.sleep(30)
+    print("Step 2: Reboot deployment")
+    page.goto("https://app.patr.cloud/")
+    page.screenshot(path=f'result3.png')
+    page.get_by_role("button", name="Infrastructure Infrastructure").click()
+    time.sleep(5)
+    page.get_by_role("button", name="VIEW DEPLOYMENTS").click()
+    time.sleep(5)
+    page.get_by_role("button", name="MANAGE DEPLOYMENT").click()
+    time.sleep(5)
+    page.screenshot(path=f'result4.png')
+    # 先Stop再Start
+    page.get_by_role("button", name="STOP").click()
+    time.sleep(60)
+    page.screenshot(path=f'result5.png')
+    page.get_by_role("button", name="START").click()
+    page.screenshot(path=f'result6.png')
+    # 预留启动加载时间
+    time.sleep(60)
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
